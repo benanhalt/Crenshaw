b77982d401cf8e45d1c188a6dcf73770e06dd9bb
Ben Anhalt <anhalt@ku.edu>
2013-07-17 14:30:42 -0500

    single char token parser

--- crenshaw.lisp
+++ crenshaw.lisp
@@ -11,7 +11,10 @@
   (error (concatenate 'string "Expected: '" expected "'")))
 
 (defun match (x)
-  (if (equal *look* x) (getchar) (expected x)))
+  (if (eq *look* x) (getchar) (expected x)))
+
+(defun addop-p (x)
+  (find x '(#\+ #\-)))
 
 (defun getname ()
   (if (alpha-char-p *look*)
@@ -25,21 +28,86 @@
       (prog1 *look* (getchar))
       (expected "Integer")))
 
-(defun emit (s)
-  (format t "    ~a" s))
+(defun emit (&rest items)
+  (format t "    ~{~a~}" items))
 
-(defun emitln (s)
-  (format t "    ~a~&" s))
+(defun emitln (&rest items)
+  (apply #'emit items)
+  (format t "~&"))
 
 (defun init ()
   (getchar))
 
+(defun ident ()
+  (let ((name (getname)))
+    (cond
+      ((eq #\( *look*)
+       (match #\()
+       (match #\))
+       (emitln "BSR " name))
+      (t (emitln "MOVE " name "(PC), D0")))))
+
+(defun factor ()
+  (cond
+    ((eq #\( *look*)
+     (match #\()
+     (expression)
+     (match #\)))
+    ((alpha-char-p *look*) (ident))
+    (t (emitln "MOVE #" (getnum) ", D0"))))
+
+(defun term ()
+  (factor)
+  (loop while (find *look* '(#\* #\/)) do
+       (emitln "MOVE D0, -(SP)")
+       (case *look*
+         (#\* (multiply))
+         (#\/ (divide)))))
+
 (defun expression ()
-  (emitln (concatenate 'string "MOVE #" (string (getnum)) ", D0")))
+  (if (addop-p *look*)
+      (emitln "CLR D0")
+      (term))
+  (loop while (addop-p *look*) do
+       (emitln "MOVE D0, -(SP)")
+       (case *look*
+         (#\+ (add))
+         (#\- (subtract)))))
+
+(defun assignment ()
+  (let ((name (getname)))
+    (match #\=)
+    (expression)
+    (emitln "LEA " name "(PC), A0")
+    (emitln "MOV D0, A0")))
+
+(defun add ()
+  (match #\+)
+  (term)
+  (emitln "ADD (SP)+, D0"))
+
+(defun subtract ()
+  (match #\-)
+  (term)
+  (emitln "SUB (SP)+, D0")
+  (emitln "NEG D0"))
+
+(defun multiply ()
+  (match #\*)
+  (factor)
+  (emitln "MULS (SP)+, D0"))
+
+(defun divide ()
+  (match #\/)
+  (factor)
+  (emitln "MOVE (SP)+, D1")
+  (emitln "DIVS D1, D0"))
 
 (defun main ()
   (init)
-  (expression))
+  (assignment)
+  (if (not (eq *look* #\newline))
+      (expected "newline")))
 
 
 
